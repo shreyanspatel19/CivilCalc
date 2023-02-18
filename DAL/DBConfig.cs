dad5c4bfff9576bc15b02c5b5cdbec2bd30173d1
jeet1s <200540107143@darshan.ac.in>
2023-02-18 21:27:57 +0530

    fix one more issue

--- DAL/DBConfig.cs
+++ DAL/DBConfig.cs
@@ -3,12 +3,16 @@ using CivilCalc.DAL.CAL.CAL_Calculator;
 using CivilCalc.DAL.CAL.CAL_NewCalculator;
 using CivilCalc.DAL.CAL.CAL_TopCalculator;
 using CivilCalc.DAL.SEC.SEC_User;
+using CivilCalc.Areas.MST_Configuration.Models;
+using CivilCalc.DAL.MST.MST_Configuration;
 
 namespace CivilCalc.DAL
 {
     public class DBConfig
     {
+        public static CAL_CategoryDAL dbCALCategory = new CAL_CategoryDAL();
         public static SEC_UserDAL dbSECUser = new SEC_UserDAL();
+        public static MST_ConfigurationDAL dbMSTConfiguration = new MST_ConfigurationDAL();
         public static CAL_CalculatorDAL dbCALCalculator = new CAL_CalculatorDAL();
         public static CAL_NewCalculatorDAL dbCALNewCalculator = new CAL_NewCalculatorDAL();
         public static CAL_TopCalculatorDAL dbCALTopCalculator = new CAL_TopCalculatorDAL();
