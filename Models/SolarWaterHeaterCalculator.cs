68b41166019f9e8914f8de0db1f075146994134c
Shreyans Padmani <200540107107@darshan.ac.in>
2023-04-04 23:02:48 +0530

    Solar Water Heater Calculator complete

---
+++ Models/SolarWaterHeaterCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CivilCalc.Models
+{
+    public class SolarWaterHeaterCalculator
+    {
+
+        [Required, Display(Name = "Nos")]
+        public int? Nos { get; set; }
+    }
+}
