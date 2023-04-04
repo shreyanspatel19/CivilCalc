443b6bb25fbf083c70f69e2c6b4ffe087761dd57
Shreyans Padmani <200540107107@darshan.ac.in>
2023-04-04 18:58:08 +0530

    tank calculation complete

---
+++ Models/TankCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CivilCalc.Models
+{
+    public class TankCalculator
+    {
+        public Int32 UnitID { get; set; }
+
+        [Required, Display(Name = "LengthA")]
+        public int? LengthA { get; set; }
+
+        [Required, Display(Name = "LengthB")]
+        [Range(0, 12, ErrorMessage = "The Length must be between 3 and 12.")]
+        public int? LengthB { get; set; }
+
+        [Required, Display(Name = "WidthA")]
+        public int? WidthA { get; set; }
+
+        [Required, Display(Name = "WidthB")]
+        public int? WidthB { get; set; }
+
+
+        [Required, Display(Name = "DepthA")]
+        public int? DepthA { get; set; }
+
+        [Required, Display(Name = "DepthB")]
+        public int? DepthB { get; set; }
+    }
+}
