6bc66db81c0fb32580aaacfe141bb6fa80716594
Shreyans Padmani <200540107107@darshan.ac.in>
2023-04-04 17:39:01 +0530

    Plywood sheets calculation  Asphalt calculation complete

---
+++ Models/PlywoodSheetsCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CivilCalc.Models
+{
+    public class PlywoodSheetsCalculator
+    {
+        public Int32 UnitID { get; set; }
+
+        [Required, Display(Name = "RoomLengthA")]
+        public int? RoomLengthA { get; set; }
+
+        [Required, Display(Name = "RoomLengthB")]
+        public int? RoomLengthB { get; set; }
+
+        [Required, Display(Name = "RoomWidthA")]
+        public int? RoomWidthA { get; set; }
+
+        [Required, Display(Name = "RoomWidthB")]
+        public int? RoomWidthB { get; set; }
+
+
+        [Required, Display(Name = "PlywoodLengthA")]
+        public int? PlywoodLengthA { get; set; }
+
+        [Required, Display(Name = "PlywoodLengthB")]
+        public int? PlywoodLengthB { get; set; }
+
+        [Required, Display(Name = "PlywoodWidthA")]
+        public int? PlywoodWidthA { get; set; }
+
+        [Required, Display(Name = "PlywoodWidthB")]
+        public int? PlywoodWidthB { get; set; }
+    }
+}
