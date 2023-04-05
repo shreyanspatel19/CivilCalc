b619715458c2f0cf6378f0e426210f9b57400e3c
Shreyans Padmani <200540107107@darshan.ac.in>
2023-04-05 10:26:13 +0530

    steel weight calculation complete

---
+++ Models/SteelWeightCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CivilCalc.Models
+{
+    public class SteelWeightCalculator
+    {
+        public Int32 UnitID { get; set; }
+
+        [Required, Display(Name = "Length")]
+        public int? LengthA { get; set; }
+
+        [Required, Display(Name = "Length")]
+        [Range(0, 11, ErrorMessage = "The Length must be between 0 and 11.")]
+        public int? LengthB { get; set; }
+
+        [Required, Display(Name = "Diameter")]
+        public int? Diameter { get; set; }
+
+        [Required, Display(Name = "Quantity")]
+        [Range(0, 11, ErrorMessage = "The Quantity must be between 0 and 11.")]
+        public int? Quantity { get; set; }
+    }
+}
