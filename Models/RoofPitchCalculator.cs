b830ab6a7ebe7b64940dd487f8ba006f7d25e97b
Shreyans Padmani <200540107107@darshan.ac.in>
2023-04-05 09:15:10 +0530

    room pitch completed

---
+++ Models/RoofPitchCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CivilCalc.Models
+{
+    public class RoofPitchCalculator
+    {
+        public Int32 UnitID { get; set; }
+
+        [Required, Display(Name = "RiseA")]
+        public int? RiseA { get; set; }
+
+        [Required, Display(Name = "RiseB")]
+        public int? RiseB { get; set; }
+
+        [Required, Display(Name = "RunA")]
+        public int? RunA { get; set; }
+
+        [Required, Display(Name = "RunB")]
+        public int? RunB { get; set; }
+    }
+}
