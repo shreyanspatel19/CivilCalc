481daa4b8ecdfbb9c87da9edc3e96fa083f15bf0
Shreyans Padmani <200540107107@darshan.ac.in>
2023-04-07 23:53:55 +0530

    Carpet Built Up Super Built Up Area Calculator

---
+++ Controllers/CarpetAreaCalculatorController.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using CivilCalc.Areas.CAL_Calculator.Models;
+using CivilCalc.Areas.LOG_Calculation.Models;
+using CivilCalc.DAL;
+using CivilCalc.DAL.LOG.LOG_Calculation;
+using CivilCalc.Models;
+using Microsoft.AspNetCore.Mvc;
+using SelectForSearch_Result = CivilCalc.DAL.CAL.CAL_Calculator.SelectForSearch_Result;
+
+namespace CivilCalc.Controllers
+{
+    public class CarpetAreaCalculatorController : Controller
+    {
+
+        #region Index
+        [Route("Quantity-Estimator/Carpet-Built-Up-Super-Built-Up-Area-Calculator")]
+        public IActionResult Index()
+        {
+            List<CivilCalc.DAL.CAL.CAL_Calculator.SelectForSearch_Result> lstCalculator = DBConfig.dbCALCalculator.SelectByURLName("/Quantity-Estimator/Carpet-Built-Up-Super-Built-Up-Area-Calculator");
+
+
+            if (lstCalculator.Count > 0)
+            {
+                foreach (var itemCalculator in lstCalculator)
+                {
+                    ViewData["HeaderName"] = itemCalculator.HeaderName;
+                    ViewData["SubHeaderName"] = itemCalculator.SubHeaderName;
+                    ViewData["CalculatorName"] = itemCalculator.CalculatorName;
+                    ViewData["CategoryName"] = itemCalculator.CategoryName;
+                    ViewBag.CalculatorIcon = itemCalculator.CalculatorIcon;
+
+                    // Meta tag
+                    ViewData["MetaTitle"] = itemCalculator.MetaTitle;
+                    ViewData["MetaKeyword"] = itemCalculator.MetaKeyword;
+                    ViewData["MetaDescription"] = itemCalculator.MetaDescription;
+                    ViewData["MetaAuthor"] = itemCalculator.MetaAuthor;
+
+                    // Meta Og tag
+                    ViewData["MetaOgTitle"] = itemCalculator.MetaOgTitle;
+                    ViewData["MetaOgType"] = itemCalculator.MetaOgType;
+                    ViewData["MetaOgDescription"] = itemCalculator.MetaOgDescription;
+                    ViewData["MetaOgUrl"] = itemCalculator.MetaOgUrl;
+                    ViewData["MetaOgImage"] = itemCalculator.MetaOgImage;
+
+                    break;
+                }
+            }
+
+            return View("CarpetAreaCalculator");
+        }
+        #endregion
+
+        #region _Calculation
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult _Calculation(CarpetAreaCalculator carpetarea)
+        {
+            var vCalculator = DBConfig.dbCALCalculator.SelectByURLName("/Quantity-Estimator/Carpet-Built-Up-Super-Built-Up-Area-Calculator").SingleOrDefault();
+            Mapper.Initialize(config => config.CreateMap<SelectForSearch_Result, CAL_CalculatorModel>());
+            var vModel = AutoMapper.Mapper.Map<SelectForSearch_Result, CAL_CalculatorModel>(vCalculator);
+
+            return PartialView("_CarpetAreaCalculatorResult", vModel);
+        }
+        #endregion
+
+    }
+}
---
+++ Models/CarpetAreaCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CivilCalc.Models
+{
+    public class CarpetAreaCalculator
+    {
+        public string? lType { get; set; }
+
+        [Required, Display(Name = "LengthFeet")]
+        public int? LengthFeet { get; set; }
+
+        [Required, Display(Name = "LengthInche")]
+        public int? LengthInche { get; set; }
+
+        [Required, Display(Name = "lengthFeet")]
+        public int? lengthFeet { get; set; }
+
+        [Required, Display(Name = "BreadthFeet")]
+        public int? BreadthFeet { get; set; }
+
+        [Required, Display(Name = "BreadthInches")]
+        public int? BreadthInches { get; set; }
+    }
+}
