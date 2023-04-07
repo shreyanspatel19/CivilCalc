7fd30e1b86deeb36542022207cbba0a6c3a38bdc
Shreyans Padmani <200540107107@darshan.ac.in>
2023-04-07 23:08:46 +0530

    Construction Cost Estimator Calculator

---
+++ Models/CostEstimatorCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CivilCalc.Models
+{
+    public class CostEstimatorCalculator
+    {
+        public int MeBuiltupmberType { get; set; }
+
+        [Required, Display(Name = "ApproxCost")]
+        public int? ApproxCost { get; set; }
+    }
+}
